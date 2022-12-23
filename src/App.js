fa8cad9de0d2b35e3035a81fae8be05097d7f79b
Rupesh kumar singh <singh.rupesh7999@gmail.com>
2022-12-23 11:55:59 +0530

    second changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <Welcome name="rupesh" src='https://images.unsplash.com/photo-1671584643473-891afcd941d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60'/>
-  <Welcome  name="aman" src='https://images.unsplash.com/photo-1671693376925-0396923c23bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60'/>
-  <Welcome  name="babu" src="https://images.unsplash.com/photo-1671642762254-4e6cbc5edbbb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60"/>
+    <Welcome name="rupesh kumar singh" src='https://images.unsplash.com/photo-1671584643473-891afcd941d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60'/>
+  <Welcome  name="aman singh" src='https://images.unsplash.com/photo-1671693376925-0396923c23bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60'/>
+  <Welcome  name="babu singh" src="https://images.unsplash.com/photo-1671642762254-4e6cbc5edbbb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60"/>
       
     </div>
   );
